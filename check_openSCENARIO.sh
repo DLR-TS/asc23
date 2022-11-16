b0aa0b756c92fda3582818a4de5a8acf6ab4fefe
Danny Behnecke <danny.behnecke@dlr.de>
2022-11-16 14:24:11 +0100

    added openscenario

---
+++ check_openSCENARIO.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#this script takes a single openSCENARIO file and checks it schema validity against the below listed versions of the openSCENARIO standard
+
+ASC23_HOME=.
+TESTFILE=$1
+
+XOSC12_SCHEMA=$ASC23_HOME/standards/openSCENARIO/openscenario-v1.2.0/Schema/OpenSCENARIO.xsd
+
+
+OUTPUT="Schema validity of ${TESTFILE}: \n"
+
+
+OUTPUT+="openSCENARIO 1.2: $(python3 $ASC23_HOME/xmlSchemeChecker.py $XODR170_SCHEMA $TESTFILE) \n"
+
+
+
+echo -e "$OUTPUT"
